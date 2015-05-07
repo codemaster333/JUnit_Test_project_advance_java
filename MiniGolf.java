855dfc5e4c4491c7b6313de3fe3f0db0ddc9159d
Alexis Herrera <aherrera17@hawkmail.hccfl.edu>
2015-05-06 20:37:50 -0400

    Java File Created
    It doesn't do anything but it sets everything up

---
+++ MiniGolf.java
@@ -0,0 +1,81 @@
+﻿public class MiniGolf 
+{
+	public class Round 
+	{
+		public static int currentScore()
+		{
+			
+			
+		}
+		
+		public static int numberOfHOlesPlayed ()
+		{
+			
+			
+		}
+		
+		public static void enterScore (int Strokes)
+		{
+			
+			
+		}
+		
+		public static int[] scoreDetail()
+		{
+			
+			
+		}
+		
+	
+	}
+
+	public static void Main 
+	{
+		public static Round newRound (String courseName, String playerName )
+		{
+
+		}		
+		
+		public static Round getRound (String player, String courseName )
+		{
+			
+		}
+		
+		public static Course getCourse (String courseName)
+		{
+			
+		}
+		
+		public static int scoreSoFar (String courseName, string playerName)
+		{
+			
+		}
+		
+		public static void enterStrokes (String courseName, String playerName, int Strokes )
+		{
+			
+		}
+		
+	}
+
+	public class Course 
+	{
+		public static String getName()
+		{
+			
+		}
+		
+		public int getNumberOfHoles ()
+		{
+			
+		}
+		
+		public int parForHole (int hole)
+		{
+			
+		}
+		
+		
+	}
+
+}
